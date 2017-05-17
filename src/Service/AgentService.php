e6e6f45f1fe259d17aa05b68243a7194c049f4d8
Ethan <ethantsien@gmail.com>
2017-05-17 15:03:40 +0800

    add new service

---
+++ src/Service/AgentService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bike\Partner\Service;
+
+use Bike\Partner\Exception\Debug\DebugException;
+use Bike\Partner\Service\AbstractService;
+
+class AgentService extends AbstractService
+{
+
+}
+ 
