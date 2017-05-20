84dac2eb1b82f81379559f8acf5d6ea9362bce2e
enoch <xingbenyong@xun-ao.com>
2017-05-20 14:03:23 +0800

    客服

--- src/Controller/CsStaff/IndexController.php
+++ src/Controller/CsStaff/IndexController.php
@@ -35,6 +35,58 @@ class IndexController extends AbstractController
      */
     public function newAction(Request $request)
     {
+        if ($request->isMethod('post')) {
+            $data = $request->request->all();
+            $csStaffService = $this->get('bike.partner.service.cs_staff');
+            try {
+                $csStaffService->createCsStaff($data);
+                return $this->jsonSuccess();
+            } catch (\Exception $e) {
+                return $this->jsonError($e);
+            }
+        }
         return array();
     }
+
+    /**
+     * @Route("/edit/{id}", name="cs_staff_edit")
+     * @Template("BikePartnerBundle:cs_staff/index:edit.html.twig")
+     */
+    public function editAction(Request $request,$id)
+    {
+        $csStaffService = $this->get('bike.partner.service.cs_staff');
+        if ($request->isMethod('post')) {
+            $data = $request->request->all();
+            try {
+                $csStaffService->editCsStaff($id,$data);
+                return $this->jsonSuccess();
+            } catch (\Exception $e) {
+                return $this->jsonError($e);
+            }
+        } else {
+            $staff = $csStaffService->getCsStaff($id);
+            $passportService = $this->container->get('bike.partner.service.passport');
+            $passport = $passportService->getPassport($id);
+            return ['staff'=>$staff,'passport'=>$passport];
+        }
+        return array();
+    }
+
+    /**
+     * @Route("/parent_staff", name="cs_staff_parent_staff")
+     */
+    public function parentStaffAction(Request $request)
+    {
+        try {
+            $level = $request->get('level');
+            $csStaffService = $this->get('bike.partner.service.cs_staff');
+            $staffs = $csStaffService->getParentStaffIdAndNameMap($level);
+            return $this->jsonSuccess($staffs);
+        } catch (\Exception $e) {
+            return $this->jsonError($e);
+        }        
+
+    }
+
+
 }
