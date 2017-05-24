a65753ba99e1d4fb2e0fa9bd7d9fe46df585faea
Ethan <ethantsien@gmail.com>
2017-05-24 16:00:02 +0800

    remove deprecated code

--- src/Service/PassportService.php
+++ src/Service/PassportService.php
@@ -84,7 +84,6 @@ class PassportService extends AbstractService
     {
         $options = [
             'cost' => 10,
-            'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
         ];
 
         return  password_hash($password, PASSWORD_BCRYPT, $options);
